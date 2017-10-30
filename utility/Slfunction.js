da77d69a9d9bc6b86d76778b4a0f662ea8bce7b7
Brandon Ng <14009481+ngbrandon1994@users.noreply.github.com>
2017-10-29 23:26:10 -0400

    forgot c in currentspotlight

--- utility/Slfunction.js
+++ utility/Slfunction.js
@@ -36,31 +36,31 @@ class Slfunction {
 		if((this.num >= 0 && this.num <=2) || (this.num >=51 && this.num <=53)){
 	        this.currentSpotlight = 'Pest Control';
 		} else if ((this.num >= 3 && this.num <= 5) || (this.num >= 54 && this.num <= 56)){
-	        this.urrentSpotlight = 'Soul Wars';
+	        this.currentSpotlight = 'Soul Wars';
 		} else if ((this.num >= 6 && this.num <= 8) || (this.num >= 45 && this.num <= 47)){
-	        this.urrentSpotlight = 'Fist Of Guthix';
+	        this.currentSpotlight = 'Fist Of Guthix';
 		} else if ((this.num >= 9 && this.num <= 11) || (this.num >= 39 && this.num <= 41)){
-	        this.urrentSpotlight = 'Barbarian Assult';
+	        this.currentSpotlight = 'Barbarian Assult';
 		} else if ((this.num >= 12 && this.num <= 14) || (this.num >= 42 && this.num <= 44)){
-	        this.urrentSpotlight = 'Conquest';
+	        this.currentSpotlight = 'Conquest';
 		} else if ((this.num >= 15 && this.num <= 17) || (this.num >= 57 && this.num <= 59)){
-	        this.urrentSpotlight = 'Fishing Trawler';
+	        this.currentSpotlight = 'Fishing Trawler';
 		} else if ((this.num >= 18 && this.num <= 20) || (this.num >= 60 && this.num <= 62)){
-	        this.urrentSpotlight = 'Great Orb Project';
+	        this.currentSpotlight = 'Great Orb Project';
 		} else if ((this.num >= 21 && this.num <= 23) || (this.num >= 63 && this.num <= 65)){
-	        this.urrentSpotlight = 'Flash Powder Factory';
+	        this.currentSpotlight = 'Flash Powder Factory';
 		} else if ((this.num >= 24 && this.num <= 26) || (this.num >= 48 && this.num <= 50) || (this.num >= 78 && this.num <= 80)){
-	        this.urrentSpotlight = 'Castle Wars!';
+	        this.currentSpotlight = 'Castle Wars!';
 		} else if ((this.num >= 27 && this.num <= 29) || (this.num >= 66 && this.num <= 68)){
-	        this.urrentSpotlight = 'Stealing Creation';
+	        this.currentSpotlight = 'Stealing Creation';
 		} else if ((this.num >= 30 && this.num <= 32) || (this.num >= 69 && this.num <= 71)){
-	        this.urrentSpotlight = 'Cabbage Facepunch Bonanza';
+	        this.currentSpotlight = 'Cabbage Facepunch Bonanza';
 		} else if ((this.num >= 33 && this.num <= 35) || (this.num >= 72 && this.num <= 74)){
-	        this.urrentSpotlight = 'Heist';
+	        this.currentSpotlight = 'Heist';
 		} else if (this.num >= 36 && this.num <= 38){
-	        this.urrentSpotlight = 'Mobilising Armies';
+	        this.currentSpotlight = 'Mobilising Armies';
 		} else if (this.num >= 75 && this.num <= 77){
-	        this.urrentSpotlight = 'Trouble Brewing';
+	        this.currentSpotlight = 'Trouble Brewing';
 		} else {
 			this.client.users.get(process.env.ownerID).send("~spotlight code error fix! asap"); 
 		}

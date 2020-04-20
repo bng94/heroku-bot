9a85070833dcfbad4ebe6535b5a82b621e43682e
Brandon Ng <ngbrandon1994@users.noreply.github.com>
2020-04-19 23:05:48 -0400

    Apparently deprecation
    discord v12.2? disconnect and reconnecting are deprecation.

--- events/reconnecting.js
+++
@@ -1,8 +0,0 @@
-module.exports = async client => {
-  // Why await here? Because the ready event isn't actually ready, sometimes
-  // guild information will come in *after* ready. 1s is plenty, generally,
-  // for all of them to be loaded.
-  await client.wait(1000);
-  // Both `wait` and `client.log` are in `./modules/functions`.
-	console.log(`Reconnecting `+client.heartBeat+` at ${new Date()}`);
-};

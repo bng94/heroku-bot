371b3d8fe68f94c258ef887a0096268cc30f6e02
Brandon Ng <ngbrandon1994@users.noreply.github.com>
2020-04-19 20:14:36 -0400

    Update mylevel.js
    added comments

--- commands/mylevel.js
+++ commands/mylevel.js
@@ -1,3 +1,4 @@
+//Checks the user's permissions, based off config.js and roles they have.
 exports.run = async (client, message, args, level) => {
   const friendly = client.config.permLevels.find(l => l.level === level).name;
   message.reply(`Your permission level is: ${level} - ${friendly}`);

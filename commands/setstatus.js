4c49843745963012f73beda1d86210c362e8c5b0
Brandon Ng <ngbrandon1994@users.noreply.github.com>
2020-04-19 20:18:45 -0400

    Update setstatus.js
    outdated

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,21 +1,21 @@
-exports.run = function(client, message, args) {
+//Sets the bot online status
+exports.run = async (client, message, args, level) => {
 	var action = message.content.split(' ').slice(1).join(' ');
-	if(message.author.id === process.env.ownerID){
-		client.user.setStatus(action);
-	} else{
-		message.channel.send("You do not have permission to invoke this command.")
-	}
+
+	if(message.channel.type != "dm") return;
+	client.user.setStatus(action);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [],
-  permLevel: 4
+  aliases: ['ss'],
+  permLevel: 10
 };
 
 exports.help = {
   name: "setstatus",
-  description: "bot's status /sarcasm",
-  usage: "setstatus"
+  category: "System",
+  description: "Set Status Online, Idle, Don't not Disturb, Invisible.",
+  usage: "setstatus <on|idle|dnd|invisible|>"
 };

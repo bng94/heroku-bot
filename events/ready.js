6cfe9513de8abeaa224ea3c1c5ff3fb8744a3ff5
Brandon Ng <14009481+bng94@users.noreply.github.com>
2022-01-19 21:32:51 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,10 @@ module.exports = {
   once: true,
   async execute(client) {
     client.user.setActivity("Bots");
-    client.log(
-      `${client.user.tag}, ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`,
-      "Ready!"
-    );
+    client.log(`Serving ${client.guilds.cache.reduce((acc, curr) => (acc += curr.memberCount), 0 )} users`,"Ready!");
+    client.log(`Servers: ${client.guilds.cache.size}`,"Ready!!");
+    client.log(`${client.user.tag}`,"Ready!!!");
+
 
     const startUpMsg = "Bot just started up!";
     const afterRestartMsg = "Bot is now back online!";

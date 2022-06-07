18c0518e25f85c08d13f0c3195a80115b80ecfea
Brandon Ng <14009481+bng94@users.noreply.github.com>
2022-06-06 23:06:01 -0400

    installed my DiscordFeaturesHandler npm package
    npm packages are awesome!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("module-alias/register");
 require("dotenv").config();
 
+const DiscordFeaturesHandler = require("discord-features-handler");
 const { Client, Collection, Intents } = require("discord.js");
 
 // !This is the new way to create the client.
@@ -20,36 +21,16 @@ const client = new Client({
   partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
 //this should be initialized strictly after client is define
-client.config = require("@root/config.js");
-
-const loadCommands = require("@handlers/loadCommands.js");
-const loadEvents = require("@handlers/loadEvents.js");
-const loadModules = require("@handlers/loadModules.js");
 
 const functions = require("@modules/functions.js");
 functions(client);
 
-client.commands = new Collection();
-client.aliases = new Collection();
-
-/**
- * init()
- * To load our commands, events, modules and login to our bot.
- */
-const init = async () => {
-  loadCommands(client);
-  loadEvents(client);
-  //used to as cache for permissions
-  client.levelCache = {};
-  for (let i = 0; i < client.config.permissions.length; i++) {
-    const thisLevel = client.config.permissions[i];
-    client.levelCache[thisLevel.name] = thisLevel.level;
-  }
-  client.login(process.env.TOKEN);
-
-  //we need to wait here to ensure bot is ready so when we need to grab channel/users for function calls in the modules(can be called features).
-  await client.wait(5000);
-  loadModules(client);
-};
-
-init();
+DiscordFeaturesHandler(client, {
+  mainDirectory: __dirname,
+  config: './config.js',
+  commandDir: 'commands',
+  eventDir: 'events',
+  modulesDir: 'modules',
+  filesToExcludeInHandlers: ['function.js', 'spotlight.js', 'restartMsg.js', 'time.js'],
+  BOT_TOKEN: process.env.TOKEN
+});

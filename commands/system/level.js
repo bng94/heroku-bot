908b6337fbfee76066d210ce46c3bab9e90f3832
Brandon Ng <14009481+bng94@users.noreply.github.com>
2022-06-19 17:12:22 -0400

    missing level param

--- commands/system/level.js
+++ commands/system/level.js
@@ -9,7 +9,7 @@ module.exports = {
 	permissions: 0,
     minArgs: 0, 
 	usage: '',
-	execute(message, args, client) {
+	execute(message, args, client, level) {
         const title = client.config.permissions.find(l => l.level === level).name;
         message.reply(`Your permission level is: ${level} - ${title}`);
     },
